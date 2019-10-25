79fb9f62b94f085f47effa62d6dc0805944d06f1
Guilherme Oliveira <guilherme.oliveira@wevo.com.br>
2019-10-24 21:25:14 -0300

    pontuacao

---
+++ Assets/Scripts/PointsCounter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsCounter : MonoBehaviour
+{
+    Text instruction;
+    [SerializeField]
+    private float tempoParaGerar;
+    private float cronometro;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        cronometro = this.tempoParaGerar;
+    }
+    void Start()
+    {
+        instruction = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double currentPoints = Convert.ToDouble(instruction.text);
+        currentPoints += 0.01;
+        instruction.text = currentPoints.ToString();
+
+        cronometro = tempoParaGerar;
+    }
+}
